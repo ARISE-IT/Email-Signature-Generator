e7afbfd831746bcbbf6410477abd5e3d79aee208
ARISE-IT <it@arisechurch.com>
2025-06-11 18:18:52 +1200

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function updateSignature() {
     <strong>${name}</strong><br>
     <em>${role}</em><br>
     ${campusText}<br>
-  <div style="margin-top: 6px;">
+ 
     <span style="font-size: 80%;">📞</span> ${phone}<br>
     <span style="font-size: 80%;">📧</span> ${email}<br>
     <span style="font-size: 80%;">📍</span> ${location}
